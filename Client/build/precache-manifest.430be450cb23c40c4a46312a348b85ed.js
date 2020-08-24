ea0fa657d476d8bd578c2e20fb5dddac18f70ba9
Sereno Balestra / Sorint Lab S.P.A <sereno.balestra@hotmail.com>
2020-08-24 14:18:29 +0200

    build

---
+++ Client/build/precache-manifest.430be450cb23c40c4a46312a348b85ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0bf111ec908cfc8afbec35bb0177df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3fca88487a2336d3d0e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3835b47fa03ff6372707",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "d3fca88487a2336d3d0e",
+    "url": "/static/js/2.14eaddbc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.14eaddbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3835b47fa03ff6372707",
+    "url": "/static/js/main.6c740b24.chunk.js"
+  },
+  {
+    "revision": "22dd261e6ae28f458a38",
+    "url": "/static/js/runtime-main.9d683513.js"
+  }
+]);
